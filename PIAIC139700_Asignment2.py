eb335e15edb246f309cf889ae8accdc5e348eb3a
Syed Shoaib Abbas <55231734+ShoaibSyedDev@users.noreply.github.com>
2020-12-30 23:47:49 +0500

    done

--- PIAIC139700_Asignment2.py
+++ PIAIC139700_Asignment2.py
@@ -1,138 +1,206 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
+# # Numpy_Assignment_2::
+
+# ## Question:1
+
+# ### Convert a 1D array to a 2D array with 2 rows?
+
+# #### Desired output::
+
+# array([[0, 1, 2, 3, 4],
+#         [5, 6, 7, 8, 9]])
+
+# In[6]:
 
 
 import numpy as np
-import matplotlib.pyplot as plt 
+array= np.arange(0,10)
+print(array)
+array.reshape(2,5)
 
 
-# In[16]:
+# ## Question:2
+
+# ###  How to stack two arrays vertically?
+
+# #### Desired Output::
+array([[0, 1, 2, 3, 4],
+        [5, 6, 7, 8, 9],
+       [1, 1, 1, 1, 1],
+       [1, 1, 1, 1, 1]])
+# In[20]:
 
 
-np.random.seed(42)
+import numpy as np
+a1=np.array([0, 1, 2, 3, 4])
+a2=np.array([5, 6, 7, 8, 9])
+a3=np.array([1, 1, 1, 1, 1])
+a4=np.array([1, 1, 1, 1, 1])
+np.vstack((a1,a2,a3,a4))
 
 
-# In[17]:
+# ## Question:3
+
+# ### How to stack two arrays horizontally?
+
+# #### Desired Output::
+array([[0, 1, 2, 3, 4, 1, 1, 1, 1, 1],
+       [5, 6, 7, 8, 9, 1, 1, 1, 1, 1]])
+       
+# In[23]:
 
 
-def enclideen_distance(x1,x2):
-    return np.sqrt(np.sum((x1-x2)**2))
+import numpy as np
+a1=np.array([0, 1, 2, 3, 4])
+a2=np.array([5, 6, 7, 8, 9])
+a3=np.array([1, 1, 1, 1, 1])
+a4=np.array([1, 1, 1, 1, 1])
+h1=np.hstack((a1,a3))
+h2=np.hstack((a2,a4))
+np.vstack((h1,h2))
 
 
-# In[24]:
+# ## Question:4
+
+# ### How to convert an array of arrays into a flat 1d array?
+
+# #### Desired Output::
+array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# In[ ]:
 
 
-class KMeans:
-    def __init__(self , k=5, max_iters=100,plot_step=False):
-        self.k=k
-        self.max_iters=max_iters
-        self.plot_step=plot_step
-        
-        self.clusters=[[] for _ in range(self.k)]
-        self.centroids=[]
-        
-    # we use unsupervised machine learning so their is no label and we not create fit method we create predict method
-    
-    def predict(self, x):
-        self.x=x
-        self.n_samples , self.n_features= x.shape
-        # initialize centroid 
-        random_sample_indx=np.random.choice(self.n_samples, self.k , replace=False)
-        self.centroids=[self.x[idx] for idx in random_sample_indx]
-        
-        # optimization 
-        for _ in range(self.max_iters):
-            # update cluster 
-            self.clusters=self._create_clusters(self.centroids)
-            if self.plot_step:
-                self.plot()
-            
-            # update centroid 
-            centroids_old =self.centroids
-            self.centroids=self._get_centroid(self.clusters)
-            if self.plot_step:
-                self.plot()
-            
-            # check if covereged
-            if self._is_converged( centroids_old , self.centroids):
-                break
-            
-            
-            # return cluster label 
-    
-        return self._get_cluster_labels(self.clusters)
-    def _get_cluster_labels(self ,clusters):
-        label=np.empty(self.n_samples)
-        for cluster_idx , cluster in enumerate(clusters):
-            for sample_idx in cluster:
-                label[sample_idx]= cluster_idx
-        return label
-        
-    def _create_clusters(self,centroids):
-        clusters=[[] for _ in range(self.k)]
-        for idx , sample in enumerate(self.x):
-            centroid_idx=self._closest_centroid(sample , centroids)
-            clusters[centroid_idx].append(idx)
-        return clusters
-    
-    
-    def _closest_centroid(self ,sample , centroids):
-        distances=[enclideen_distance(sample ,point) for point  in centroids]
-        closest_idx=np.argmin(distances)
-        return closest_idx
-    
-    def _get_centroid(self,clusters):
-        centroids=np.zeros((self.k , self.n_features))
-        for cluster_idx , cluster in enumerate(clusters):
-            cluster_mean=np.mean(self.x[cluster],axis=0)
-            centroids[cluster_idx]=cluster_mean
-        return centroids
-                           
-    def _is_converged(self,  centroids_old , centroids):
-        distances=[enclideen_distance(centroids_old[i], centroids[i]) for i in range(self.k)]
-        return sum(distances)==0
-    
-    def plot(self):
-        fig , ax =plt.subplot(figsize=(12,8))
-        for i , index in enumerate(self.clusters):
-            point=self.x[index].T 
-            ax.scatter(*point)
-        for point in self.centroids:
-            ax.scattertter(*point, marker="X", color="black", linewidth=2)
-        plt.show()
 
 
-# In[25]:
 
+# ## Question:5
 
-from sklearn.datasets import make_blobs
+# ### How to Convert higher dimension into one dimension?
 
-x , y=make_blobs(centers=4, n_samples=500, n_features=2, shuffle=True , random_state=42) 
-
-
+# #### Desired Output::
+array([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
 # In[27]:
 
 
-print(x.shape)
-clusters=len(np.unique(y))
-print(clusters)
-kmean = KMeans(k=clusters, max_iters=150, plot_step=False )
-y_pred =kmean.predict(x)
-y_pred
+a1=np.array([0, 1, 2, 3, 4,5, 6, 7, 8, 9])
+# first convert in 2D
+a1.reshape(2,5)
+a1.flatten()
 
 
-# In[ ]:
+# ## Question:6
+
+# ### Convert one dimension to higher dimension?
+
+# #### Desired Output::
+array([[ 0, 1, 2],
+[ 3, 4, 5],
+[ 6, 7, 8],
+[ 9, 10, 11],
+[12, 13, 14]])
+# In[35]:
 
 
+a1=np.array([0, 1, 2, 3, 4,5, 6, 7, 8, 9,10,11,12,13,14])
+a1.reshape(5,3)
 
 
+# ## Question:7
 
-# In[ ]:
+# ### Create 5x5 an array and find the square of an array?
+
+# In[40]:
 
 
+a1=np.random.random((5,5))
+print(a1)
+np.square(a1)
 
+
+# ## Question:8
+
+# ### Create 5x6 an array and find the mean?
+
+# In[42]:
+
+
+a1=np.random.random((5,6))
+print(a1)
+np.mean(a1)
+
+
+# ## Question:9
+
+# ### Find the standard deviation of the previous array in Q8?
+
+# In[43]:
+
+
+np.std(a1)
+
+
+# ## Question:10
+
+# ### Find the median of the previous array in Q8?
+
+# In[44]:
+
+
+np.median(a1)
+
+
+# ## Question:11
+
+# ### Find the transpose of the previous array in Q8?
+
+# In[45]:
+
+
+np.transpose(a1)
+
+
+# ## Question:12
+
+# ### Create a 4x4 an array and find the sum of diagonal elements?
+
+# In[46]:
+
+
+a1=np.random.random((4,4))
+print(a1)
+np.trace(a1)
+
+
+# ## Question:13
+
+# ### Find the determinant of the previous array in Q12?
+
+# In[47]:
+
+
+np.linalg.det(a1)
+
+
+# ## Question:14
+
+# ### Find the 5th and 95th percentile of an array?
+
+# In[50]:
+
+
+print(np.percentile(a1,5))
+print(np.percentile(a1,95))
+
+
+# ## Question:15
+
+# ### How to find if a given array has any null values?
+
+# In[51]:
+
+
+np.isnan(a1)
 
 
 # In[ ]:
